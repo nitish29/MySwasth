ca7e522dd91232c4ec699ee48b1982bd0e107a52
Anant Mittal <mittal.anant@gmail.com>
2014-08-03 00:50:34 +0530

    Test Form

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -64,6 +64,21 @@ myswasthControllers.controller('TestCtrl', ['$scope', '$http',
     });
 }]);
 
+myswasthControllers.controller('TestRecordCtrl', ['$scope', '$http',
+  function ($scope, $http) {
+    $http.get('data/test-form.json').success(function(data) {
+      $scope.testForm = data;
+      $scope.selectedOption = data[0];
+    });
+    
+    $http.get('data/tests.json').success(function(data) {
+        $scope.testForm2 = data;
+      });
+    $scope.getValues = function(id) {
+        return $scope.testForm[id];
+      }
+}]);
+
 myswasthControllers.controller('HistoryCtrl', ['$scope', '$http',
   function ($scope, $http) {
     $http.get('data/homeicons.json').success(function(data) {
